cf07a10bf16da63dc867b8863e15200888e258fa
Roelan <roelangeil.utech@gmail.com>
2018-10-08 10:55:06 +0800

    Remove IDs of SerialNumber. (Inventory)

--- assets/js/inventory/currentInventory.js
+++ assets/js/inventory/currentInventory.js
@@ -265,7 +265,7 @@ var dtCurrentInventory = {
     },
     update: function(id){
     	 var $btn     = $("button[type='submit']");
-		 var serialnum = $("#txtCurInvntSerial").val();
+		 var serialnum = $(".txtCurInvntSerial").val();
 		 var brand 	   = $("#slctCurInvntBrands option:selected").val();
 		 var model     = $("#slctCurInvntModel").chosen().val();
 		 var type      = $("#slctCurInvntType option:selected").val();
@@ -305,7 +305,7 @@ var dtCurrentInventory = {
 	                success: function(data, xhr, status){
 	                	$.each(data.aaData, function(i,val){
 	                		 $("#hdnCurInvntId").val(val.id);
-	                		 $("#txtCurInvntSerial").val(val.serialnumber);
+	                		 $(".txtCurInvntSerial").val(val.serialnumber);
 	                		 $("#hdnOldCurInvntSerial").val(val.serialnumber);
 							 $("#slctCurInvntBrands").val(val.id_brand).trigger('change');
 							 $("#slctCurInvntModel").val(val.model).trigger('chosen:updated');
@@ -524,7 +524,7 @@ var dtCurrentInventory = {
 			    e.preventDefault();
 			    if(max_sn <= 10){
 			         $(container).append('<div class="input-group">'+
-			            '<input type="text" class="form-control txtCurInvntSerial" id="txtCurInvntSerial" name="txtCurInvntSerial" placeholder="S/N '+max_sn+'"/>'+
+			            '<input type="text" class="form-control txtCurInvntSerial" name="txtCurInvntSerial" placeholder="S/N '+max_sn+'"/>'+
 			            '<span class="input-group-btn">'+
 			                '<button class="btn btn-danger btn-xs delete-sn" type="button">x</button>'+
 			           '</span></div>').focus();
