12ad165c8880da2f17022d4b18a49a2960a2477e
Roelan <roelangeil.utech@gmail.com>
2019-06-22 10:04:18 +0800

    Remove count BN from manually entry in Inventory Reports.

--- assets/php/inventory/reports/all-stocks.php
+++ assets/php/inventory/reports/all-stocks.php
@@ -33,23 +33,30 @@ $db = Database::getInstance();
 // WHERE im.is_delete = 0 AND (im.id_status = 0 OR s.status_type = "IN")
 // GROUP BY im.id_branch,im.model ORDER BY id_branch_sort, model) X GROUP BY branch, model ORDER BY id_branch_sort');
 
+// $db->selectQuery2('SELECT branch, brand, model, id_branch_sort, SUM(id_condition = 1) AS bn, SUM(id_condition = 2) AS rf, COUNT(*) AS total_model FROM (
+// SELECT imai.serialnumber, imai.id_brand AS brand, imai.model, br.branch_name AS branch, br.id AS id_branch_sort,
+//  1 AS id_condition
+//  FROM tbl_invnt_machines_auto_import imai
+//  LEFT JOIN tbl_invnt_machines_saploc ims ON imai.location = ims.branch
+//  LEFT JOIN tbl_branch br ON ims.equiv_id_branch = br.id
+//  UNION ALL
+// SELECT im.serialnumber, br.brand_name AS brand, mo.model_name AS model, brc.branch_name AS branch, brc.id AS id_branch_sort,
+//  im.id_condition
+//  FROM tbl_invnt_machines im
+// LEFT JOIN tbl_branch brc ON im.id_branch = brc.id
+// LEFT JOIN tbl_brands br ON im.id_brand = br.id
+// LEFT JOIN tbl_model mo ON im.model = mo.id
+// LEFT JOIN tbl_invnt_status s ON im.id_status = s.id
+// WHERE NOT EXISTS(SELECT serialnumber FROM tbl_invnt_machines_auto_import ina WHERE im.serialnumber = ina.serialnumber) 
+// AND im.is_delete = 0 AND (im.id_status = 0 OR s.status_type = "IN")
+// ) X GROUP BY branch, model ORDER BY brand, model');
+
 $db->selectQuery2('SELECT branch, brand, model, id_branch_sort, SUM(id_condition = 1) AS bn, SUM(id_condition = 2) AS rf, COUNT(*) AS total_model FROM (
 SELECT imai.serialnumber, imai.id_brand AS brand, imai.model, br.branch_name AS branch, br.id AS id_branch_sort,
  1 AS id_condition
  FROM tbl_invnt_machines_auto_import imai
  LEFT JOIN tbl_invnt_machines_saploc ims ON imai.location = ims.branch
- LEFT JOIN tbl_branch br ON ims.equiv_id_branch = br.id
- UNION ALL
-SELECT im.serialnumber, br.brand_name AS brand, mo.model_name AS model, brc.branch_name AS branch, brc.id AS id_branch_sort,
- im.id_condition
- FROM tbl_invnt_machines im
-LEFT JOIN tbl_branch brc ON im.id_branch = brc.id
-LEFT JOIN tbl_brands br ON im.id_brand = br.id
-LEFT JOIN tbl_model mo ON im.model = mo.id
-LEFT JOIN tbl_invnt_status s ON im.id_status = s.id
-WHERE NOT EXISTS(SELECT serialnumber FROM tbl_invnt_machines_auto_import ina WHERE im.serialnumber = ina.serialnumber) 
-AND im.is_delete = 0 AND (im.id_status = 0 OR s.status_type = "IN")
-) X GROUP BY branch, model ORDER BY id_branch_sort');
+ LEFT JOIN tbl_branch br ON ims.equiv_id_branch = br.id) X GROUP BY branch,model ORDER BY brand, model');
 	$row = $db->getFields(); //Get all rows
 	$data = array();
 
