6b999cbf37b8da778080a55c2633521e3e79518d
Roelan <roelangeil.utech@gmail.com>
2019-07-18 08:58:57 +0800

    Fix no technician display.

--- assets/php/machine/pmHistory.php
+++ assets/php/machine/pmHistory.php
@@ -16,8 +16,9 @@ $requestData= $_REQUEST;
 $conn->selectQuery('pm.*','tbl_pm_machines pm
 LEFT JOIN tbl_pm_schedule ps ON pm.pm_number = ps.pm_number
 LEFT JOIN tbl_company com ON pm.company_id = com.id
-LEFT JOIN tbl_accounts ac ON ps.technician = ac.id
-WHERE pm.mif_id ='.$requestData['mif_id'].' AND ps.status = "close"');
+LEFT JOIN tbl_pm_technician pt ON pm.pm_number = pt.pm_number
+LEFT JOIN tbl_accounts ac ON pt.technician = ac.id
+WHERE pm.mif_id ='.$requestData['mif_id'].' AND ps.status = "close" GROUP BY pm.pm_number');
 $totalData = $conn->getNumRows(); //getting total number records without any search.
 $conn->row_count = 0;
 $conn->fields = null;
@@ -32,8 +33,9 @@ $conn->selectQuery('pm.*',
 	'tbl_pm_machines pm
 	LEFT JOIN tbl_pm_schedule ps ON pm.pm_number = ps.pm_number
 	LEFT JOIN tbl_company com ON pm.company_id = com.id
-	LEFT JOIN tbl_accounts ac ON ps.technician = ac.id
-	WHERE pm.mif_id = '.$requestData['mif_id'].' '.$search.' AND ps.status = "close"');
+	LEFT JOIN tbl_pm_technician pt ON pm.pm_number = pt.pm_number
+	LEFT JOIN tbl_accounts ac ON pt.technician = ac.id
+	WHERE pm.mif_id = '.$requestData['mif_id'].' '.$search.' AND ps.status = "close" GROUP BY pm.pm_number');
 
 	$conn->fields = null;
 	$totalFiltered  = $conn->getNumRows(); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
@@ -42,12 +44,13 @@ else{
 	$totalFiltered = $totalData;
 }
 
-$conn->selectQuery('com.company_name, ps.schedule_date, CONCAT(ac.firstname," ", ac.lastname) AS technician, pm.remarks, pm.page_count, pm.time_in, pm.time_out',
+$conn->selectQuery('com.company_name, ps.schedule_date, GROUP_CONCAT(CONCAT(ac.firstname," ", ac.lastname) SEPARATOR "<br>") AS technician, pm.remarks, pm.page_count, pm.time_in, pm.time_out',
 	'tbl_pm_machines pm
 	LEFT JOIN tbl_pm_schedule ps ON pm.pm_number = ps.pm_number
 	LEFT JOIN tbl_company com ON pm.company_id = com.id
-	LEFT JOIN tbl_accounts ac ON ps.technician = ac.id
-	WHERE pm.mif_id ="'.$requestData['mif_id'].'" AND ps.status = "close" '.$search.'  ORDER BY pm.id DESC LIMIT '.$requestData['start'].' ,'.$requestData['length'].'');
+	LEFT JOIN tbl_pm_technician pt ON pm.pm_number = pt.pm_number
+	LEFT JOIN tbl_accounts ac ON pt.technician = ac.id
+	WHERE pm.mif_id ="'.$requestData['mif_id'].'" AND ps.status = "close" GROUP BY pm.pm_number '.$search.'  ORDER BY pm.id DESC LIMIT '.$requestData['start'].' ,'.$requestData['length'].'');
 $row = $conn->getFields(); //Get all rows
 
 
