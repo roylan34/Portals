5abe265c6fb417bee2e9d2de4f5f2af44765ea9a
Roelan <roelangeil.utech@gmail.com>
2020-02-06 11:31:29 +0800

    Fix cant't add model in Settings MIF

--- assets/php/inventory/settings/models.php
+++ assets/php/inventory/settings/models.php
@@ -27,7 +27,7 @@ if(Utils::getIsset('action')){
 	$action_validate = Utils::getValue('action_validate');
 
 	switch ($action) {
-		case 'add_':
+		case 'add':
 				$db->insertQuery('tbl_model','id_brand,model_name,id_category,id_type',
 												'"'.$brand.'",
 												"'.Utils::uppercase($model_name).'",
